5aaa9bd5721910f887f3c1d451c0dc03a3a98b14
TomaszZayl <42319689+TomaszZayl@users.noreply.github.com>
2018-09-25 13:28:21 +0200

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package Algorytmy;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        SortRandomNumbersInFile readFile = new SortRandomNumbersInFile();
+        readFile.printToArray();
+        readFile.printToConsole();
+    }
+}
