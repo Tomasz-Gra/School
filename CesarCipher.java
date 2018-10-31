01ddf17332611f8d98039b6f0d307fec5ff7c5a3
TomaszZayl <42319689+TomaszZayl@users.noreply.github.com>
2018-10-31 16:33:01 +0100

    Update CesarCipher.java

--- CesarCipher.java
+++ CesarCipher.java
@@ -4,51 +4,67 @@ import java.util.Scanner;
 
 class CesarCipher {
 
-    public static void main(String[] args)
-    {
+    private StringBuffer encryptedText = new StringBuffer();
+    private String userInput = "";
+
+    public static void main(String[] args) {
         CesarCipher cesarCipher = new CesarCipher();
         cesarCipher.execute();
+        cesarCipher.changeToBinary();
     }
 
     private void execute() {
+
         Scanner askForText = new Scanner(System.in);
 
-        System.out.println("Please enter text you want to convert: ");
-        String userInput = askForText.nextLine();
-        System.out.println("Please enter number that you want to shift by: ");
+        System.out.print("Please enter text you want to convert: ");
+        userInput = askForText.nextLine();
+        System.out.print("Please enter number that you want to shift by: ");
         int shiftByNumber = askForText.nextInt();
 
-        System.out.println("Text entered: " + userInput);
-        System.out.println("Shift by: " + shiftByNumber);
-        System.out.println("Encrypted text: " + performEncryption(userInput, shiftByNumber));
+        System.out.format("\nText entered: %s\nShift by: %d\nEncrypted text: %s\n", userInput, shiftByNumber, performEncryption(userInput, shiftByNumber));
     }
 
-    private StringBuffer performEncryption(String userTextInput, int shiftByNumber)
-    {
-        StringBuffer encryptedText = new StringBuffer();
-
-        for (int i = 0; i < userTextInput.length(); i++)
-        {
-            if (Character.isUpperCase(userTextInput.charAt(i)))
-            {
-                if (userTextInput.charAt(i) == ' ') {
-                    encryptedText.append(' ');
+    private StringBuffer performEncryption(String userTextInput, int shiftByNumber) {
+        for (int i = 0; i < userTextInput.length(); i++) {
+            if (Character.isLetter(userInput.charAt(i))) {
+                if (Character.isUpperCase(userTextInput.charAt(i))) {
+                    if (userTextInput.charAt(i) == ' ') {
+                        encryptedText.append(' ');
+                    } else {
+                        char characterAtString = (char) (((int) userTextInput.charAt(i) + shiftByNumber - 65) % 26 + 65);
+                        encryptedText.append(characterAtString);
+                    }
                 } else {
-                    char characterAtString = (char) (((int) userTextInput.charAt(i) + shiftByNumber - 65) % 26 + 65);
-                    encryptedText.append(characterAtString);
+                    if (userTextInput.charAt(i) == ' ') {
+                        encryptedText.append(' ');
+                    } else {
+                        char characterAtString = (char) (((int) userTextInput.charAt(i) + shiftByNumber - 97) % 26 + 97);
+                        encryptedText.append(characterAtString);
+                    }
                 }
-            }
-            else
-            {
+            } else {
                 if (userTextInput.charAt(i) == ' ') {
                     encryptedText.append(' ');
                 } else {
-                    char characterAtString = (char) (((int) userTextInput.charAt(i) + shiftByNumber - 97) % 26 + 97);
-                    encryptedText.append(characterAtString);
+                    encryptedText.append((char) (255 - userInput.charAt(i)));
                 }
             }
         }
 
         return encryptedText;
+    }
+
+    private void changeToBinary() {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (!Character.isLetter(userInput.charAt(i))) {
+                System.out.format("\nCharacter that is not letter: %s", userInput.charAt(i));
+                System.out.format("\nNegated binary string: %s", Integer.toBinaryString(255 - userInput.charAt(i)));
+                System.out.format("\nBinary ASCII code: %d", 255 - userInput.charAt(i));
+
+                System.out.println("\nCharacter of negated binary ASCII code: " + (char) (255 - userInput.charAt(i)));
+                encryptedText.append((char) (255 - userInput.charAt(i)));
+            }
+        }
     }
 }
